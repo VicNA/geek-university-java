a289f39a13bd88c60e1a55f3b8fc0879f77fbd1c
VicN <vnikolaeff989@gmail.com>
2021-06-27 17:20:43 +0300

    HomeWork1

---
+++ JavaLevel3/HomeWork1/src/main/java/ru/geekbrains/javalevel3/lesson1/task3/Apple.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.javalevel3.lesson1.task3;
+
+public class Apple extends Fruit {
+
+    public Apple() {
+       this.weight = 1f;
+    }
+
+    @Override
+    public float getWeight() {
+        return weight;
+    }
+}
---
+++ JavaLevel3/HomeWork1/src/main/java/ru/geekbrains/javalevel3/lesson1/task3/Orange.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.javalevel3.lesson1.task3;
+
+public class Orange extends Fruit {
+
+    public Orange() {
+        this.weight = 1.5f;
+    }
+
+    @Override
+    public float getWeight() {
+        return weight;
+    }
+}
